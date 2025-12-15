b7ced29cbba0fdf1f741a27353cad188a1337bd0
chen yuxiang <yuxiangchen10@gmail.com>
2025-12-15 19:37:37 +0800

    improve quick start implementation with campaign start hook

--- SubModule/WelcomeMessage.cs
+++ SubModule/WelcomeMessage.cs
@@ -3,14 +3,13 @@ using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Library;
-using TaleWorlds.CampaignSystem.GameComponents;
-using TaleWorlds.CampaignSystem.CharacterCreationContent;
-using TaleWorlds.CampaignSystem.CharacterCreation;
 
 namespace NordThrowingAxeMod
 {
     public class WelcomeMessageSubModule : MBSubModuleBase
     {
+        private static bool _hasGivenQuickStartGold = false;
+
         protected override void OnSubModuleLoad()
         {
             base.OnSubModuleLoad();
@@ -24,12 +23,18 @@ namespace NordThrowingAxeMod
         protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
         {
             base.OnGameStart(game, gameStarterObject);
+        }
+
+        protected override void OnCampaignStart(Game game, object starterObject)
+        {
+            base.OnCampaignStart(game, starterObject);
             
-            // 如果是新游戏，给玩家10万金币（快速开局）
-            if (game.GameType is CampaignGameManager campaignGame)
+            // 在新战役开始时给玩家10万金币（快速开局）
+            if (Campaign.Current != null && !_hasGivenQuickStartGold)
             {
-                Campaign.Current?.AddMoneyToPlayerParty(100000);
+                Campaign.Current.AddMoneyToPlayerParty(100000);
                 InformationManager.DisplayMessage(new InformationMessage("快速开局：已获得 100,000 金币"));
+                _hasGivenQuickStartGold = true;
             }
         }
     }
