83d313e9f4c7c951042e5451a2441df1a159ce5d
chen yuxiang <yuxiangchen10@gmail.com>
2025-12-15 19:36:35 +0800

    fix imperial crossbow cavalry upgrade path and add quick start gold bonus

--- SubModule/WelcomeMessage.cs
+++ SubModule/WelcomeMessage.cs
@@ -1,7 +1,11 @@
 using System;
 using TaleWorlds.Core;
-using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Library;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.CharacterCreationContent;
+using TaleWorlds.CampaignSystem.CharacterCreation;
 
 namespace NordThrowingAxeMod
 {
@@ -20,17 +24,13 @@ namespace NordThrowingAxeMod
         protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
         {
             base.OnGameStart(game, gameStarterObject);
-        }
-
-        public override void OnGameInitializationFinished(Game game)
-        {
-            base.OnGameInitializationFinished(game);
             
-            // 显示欢迎消息（简单版本 - 只在消息栏显示）
-            InformationManager.DisplayMessage(new InformationMessage(
-                "欢迎使用 Nord Throwing Axe Mod！",
-                Colors.Green
-            ));
+            // 如果是新游戏，给玩家10万金币（快速开局）
+            if (game.GameType is CampaignGameManager campaignGame)
+            {
+                Campaign.Current?.AddMoneyToPlayerParty(100000);
+                InformationManager.DisplayMessage(new InformationMessage("快速开局：已获得 100,000 金币"));
+            }
         }
     }
 }
